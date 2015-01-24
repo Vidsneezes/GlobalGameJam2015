3ff5411eb7774e83bb30ce1446e57e61b6560a65
Vidsneezes <moraleszez@gmail.com>
2015-01-24 10:24:13 -0600

    more movement stuff

---
+++ Assets/Scripts/SanityObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SanityObject : MonoBehaviour
+{
+
+		private PlayerInteraction playerInteraction;
+
+		// Use this for initialization
+		void Start ()
+		{
+				playerInteraction = GameObject.Find ("MainPlayer").GetComponent<PlayerInteraction> ();
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
