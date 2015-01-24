2ecf59ed2b1d1dd92e0d25c2ca6a7c82ec7e6b43
Vidsneezes <moraleszez@gmail.com>
2015-01-24 10:46:18 -0600

    message prompter

---
+++ Assets/Scripts/Interactable/MessageInformer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MessageInformer : MonoBehaviour
+{
+		[HideInInspector]
+		public string
+				message;
+		[HideInInspector]
+		public TextMesh
+				textMesh;
+
+		// Use this for initialization
+		void Start ()
+		{
+				textMesh = GetComponent<TextMesh> ();
+		}
+	
+		public void DisplayMessage (string message)
+		{
+				textMesh.color = new Color (1, 1, 1, 1);
+				textMesh.text = message;
+				Invoke ("CloseMessage", 1.2f);
+		}
+		
+		void CloseMessage ()
+		{
+				textMesh.color = new Color (1, 1, 1, 0);
+		}
+}
