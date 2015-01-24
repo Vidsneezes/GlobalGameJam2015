88bac34945f008dd71593505ac3a6993b40478b2
Vidsneezes <moraleszez@gmail.com>
2015-01-24 17:46:42 -0600

    spin world sun spot rotates

---
+++ Assets/Scripts/Utils/SpinMyWorld.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinMyWorld : MonoBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				float temp = transform.eulerAngles.y + 10 * Time.deltaTime;
+				transform.eulerAngles = new Vector3 (transform.eulerAngles.x, temp, transform.eulerAngles.z);
+		}
+}
