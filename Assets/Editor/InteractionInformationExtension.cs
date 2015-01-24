eb4b527850d9afb115fb7cf82f77cd5f351cd41d
Vidsneezes <moraleszez@gmail.com>
2015-01-24 11:09:00 -0600

    extension for message prompter

---
+++ Assets/Editor/InteractionInformationExtension.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(InteractionInformation))]
+public class InteractionInformationExtension :  Editor
+{
+
+		public override void OnInspectorGUI ()
+		{
+		
+				InteractionInformation inform = (InteractionInformation)target;
+				DrawDefaultInspector ();
+		}
+}
