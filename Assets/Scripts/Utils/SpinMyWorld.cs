f63640e96b72abeceb37cf8c76d293a8b9d87cdb
Vidsneezes <moraleszez@gmail.com>
2015-01-24 19:00:44 -0600

    lighting enginge yea

--- Assets/Scripts/Utils/SpinMyWorld.cs
+++ Assets/Scripts/Utils/SpinMyWorld.cs
@@ -3,17 +3,19 @@ using System.Collections;
 
 public class SpinMyWorld : MonoBehaviour
 {
-
+		private PlayerInteraction playerInteraction;
+		public float speed;
+	
 		// Use this for initialization
 		void Start ()
 		{
-	
+				playerInteraction = GameObject.Find ("MainPlayer").GetComponent<PlayerInteraction> ();
 		}
 	
 		// Update is called once per frame
 		void Update ()
 		{
-				float temp = transform.eulerAngles.y + 10 * Time.deltaTime;
+				float temp = transform.eulerAngles.y + speed * (1 - (playerInteraction.sanity / 100)) * Time.deltaTime;
 				transform.eulerAngles = new Vector3 (transform.eulerAngles.x, temp, transform.eulerAngles.z);
 		}
 }
