05da8d45b7cdbe52e4bb267f5926b1b06e0b0cba
Vidsneezes <moraleszez@gmail.com>
2015-01-25 10:23:35 -0600

    alarm sound, start page

---
+++ Assets/Scripts/Ani/RotateTowards.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateTowards : MonoBehaviour
+{
+
+		public float moveToPosition;
+		RectTransform rectTransform;
+	
+		// Use this for initialization
+		void Start ()
+		{
+				rectTransform = GetComponent<RectTransform> ();
+				Debug.Log (rectTransform.rotation);
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				rectTransform.eulerAngles = Vector3.MoveTowards (rectTransform.eulerAngles, Vector3.zero, moveToPosition);
+		}
+}
---
+++ Assets/Scripts/Interactable/StartGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour
+{
+
+		public void StartGameAction ()
+		{
+				Application.LoadLevel (1);
+		}
+}
