ec919e263dc42c9c4c118fd77ec45b6560f90447
Vidsneezes <moraleszez@gmail.com>
2015-01-25 10:58:52 -0600

    added alarm clock

---
+++ Assets/Scripts/Player/PlayerCollectionInteract.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using InControl;
+
+
+public class PlayerCollectionInteract : MonoBehaviour
+{
+
+		private float
+				sanity;
+	
+		private GameObject targetDistance;
+	
+		private GameObject cameraPosition;
+	
+		public int interactionDistance;
+	
+		public float interactionWait;
+		public bool justInteracted;
+		
+		public GameObject alarmClock;
+		
+		
+	
+		// Use this for initialization
+		void Start ()
+		{
+		
+				targetDistance = GameObject.Find ("TargetDistance");
+				cameraPosition = GameObject.Find ("Main Camera");
+				
+		
+		}
+	
+		void EnableInteraction ()
+		{
+				justInteracted = false;
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				var inputeDevice = InputManager.ActiveDevice;
+		
+				if ((inputeDevice.Action1 || inputeDevice.RightTrigger) && targetDistance != null) {
+						Debug.DrawLine (cameraPosition.transform.position, targetDistance.transform.position, Color.blue, interactionDistance);
+						RaycastHit info = new RaycastHit ();
+						if (Physics.Linecast (cameraPosition.transform.position, targetDistance.transform.position, out info, interactionDistance) && justInteracted == false) {
+								InteractionInformation temp = info.collider.gameObject.GetComponent<InteractionInformation> ();
+								if (temp != null) {
+										if (info.collider.gameObject.name == alarmClock.name) {
+												alarmClock.audio.Stop ();
+										}
+								}
+						}
+			
+			
+				}
+		}
+		
+}
